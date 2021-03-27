986ff4f5d9b32e73169a883709ec5fb9f2c6c96e
nikkig124 <nikkig124@gmail.com>
2021-03-27 12:05:38 -0500

    changes

--- src/components/CommentShow.js
+++ src/components/CommentShow.js
@@ -2,13 +2,21 @@ import React, { useEffect, useState } from 'react';
 import { Redirect, useLocation } from 'react-router-dom';
 import CommentModel from "../models/comment"
 
+const playlist = {
+    anger: '20Bbvjfo6UGSieemnaa62R',
+    disgust: '6SugLh3r9BscE1Srn5Rf6B',
+    fear:'6SugLh3r9BscE1Srn5Rf6B',
+    joy: '6SugLh3r9BscE1Srn5Rf6B',
+    sadness:'6SugLh3r9BscE1Srn5Rf6B',
+}
+
 const CommentShow = (props) => {
     const [comment, setComment] = useState(null)
     const [editMode, setEditMode] = useState(false)
-    const [url, setUrl] = useState(null)
+    const [url, setUrl] = useState(playlist[props.emotion])
 
     const location = useLocation()
-    
+
     const getInfo = async() => {
         const info = await CommentModel.show()
         setComment(info)
@@ -25,7 +33,7 @@ const CommentShow = (props) => {
         console.log(res)
         props.history.push('/')
     }
-    
+
     const handleUpdate = async () => {
         const res = await CommentModel.update(comment)
         console.log(res)
@@ -38,7 +46,7 @@ const CommentShow = (props) => {
 
     const displayInfo = (
         <div>
-            { comment ? <h4>{ comment }</h4> : <h4>No comment</h4>} 
+            { comment ? <h4>{ comment }</h4> : <h4>No comment</h4>}
         </div>
     )
 
@@ -59,7 +67,7 @@ const CommentShow = (props) => {
     const editInfo = (
         <div>Edit
             <div>
-                <input 
+                <input
                     type="text"
                     name="comment"
                     value={ comment.content}
@@ -73,11 +81,11 @@ const CommentShow = (props) => {
         <div>
             <iframe src={"https://open.spotify.com/embed/playlist/"+ url} width="300" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
             { editMode ? editInfo : displayInfo }
-                <div>           
+                <div>
                     <button onClick={ handleDelete } className="deleteButton">Delete</button>
                     <button onClick={ editComment } className="editButton">{ editMode ? "Save Changes" : "Edit" }</button>
-                </div>    
-        </div> 
+                </div>
+        </div>
     );
 }
 
--- src/components/CreateComment.js
+++ src/components/CreateComment.js
@@ -1,71 +1,73 @@
-import React, { Component, useState } from 'react'
-import CommentModel from '../models/comment'
-import createComment from './createComment.css'
+import React, { Component, useState } from 'react';
+import CommentModel from '../models/comment';
+import createComment from './createComment.css';
 import axios from 'axios';
 
 class CreateComment extends Component {
     state = {
-        content: "",
+        content: '',
         completed: false,
-        url: "",
-    }
+        url: '',
+        tone: null,
+    };
     // post to tone analyzer and return the tone
     // tone must be stored in EMOTION MODEL = {mood: string}
     //
-    handleSubmit = (event) => {
-        event.preventDefault()
+    handleSubmit = event => {
+        event.preventDefault();
 
-        const text = this.state.content
+        const text = this.state.content;
         axios({
             method: 'post',
-            url: "http://localhost:4000/api/v1/emotion",
+            url: 'http://localhost:4000/api/v1/emotion',
             data: {
                 text,
             },
         })
-        .then(tone => {
-            console.log(tone);
-            // this.state.url = this.playlist.tone
-            this.setState({url: this.tone}) // coming back undefined
-        }).catch(err => console.log("TONE ERROR", err))
-        CommentModel.create(this.state)
-        .then(data => {
+            .then(tone => {
+                console.log(tone);
+
+                // this.state.url = this.playlist.tone
+                this.setState({ emotion: tone }); // coming back undefined
+            })
+            .catch(err => console.log('TONE ERROR', err));
+
+        CommentModel.create(this.state).then(data => {
             this.props.history.push({
                 pathname: `/comment/${data.id}`,
                 // state: { comment: data, url: this.state.url },
-                state: { comment: data, emotions: this.state.url }
-            })
-        })
-    }
+                state: { comment: data, emotion: this.state.emotion },
+            });
+        });
+    };
 
-
-    handleChange = (event) => {
+    handleChange = event => {
         if (event.target.type !== 'text') {
-            this.setState({ completed: !this.state.completed })
+            this.setState({ completed: !this.state.completed });
         }
         this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
+            [event.target.name]: event.target.value,
+        });
+    };
 
-    render(){
+    render() {
         return (
-            <div className="form">
-                <form onSubmit={ this.handleSubmit } >
-                    <div className="form-input">
+            <div className='form'>
+                <form onSubmit={this.handleSubmit}>
+                    <div className='form-input'>
                         {/* Controlled Input */}
                         <input
-                        type="text"
-                        name="content"
-                        onChange={ this.handleChange }
-                        value={ this.state.content }
-                        className="inputForm"
+                            type='text'
+                            name='content'
+                            onChange={this.handleChange}
+                            value={this.state.content}
+                            className='inputForm'
                         />
                     </div>
-                    <input type="submit" value="Curate" className="button"/>
+                    <input type='submit' value='Curate' className='button' />
                 </form>
             </div>
-        )
+        );
     }
 }
 
